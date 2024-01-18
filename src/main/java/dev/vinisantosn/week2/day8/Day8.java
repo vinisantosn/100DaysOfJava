c38f0e35210818bffdbec1221aef895e468efcff
Vinicius Santos <vinisantoosk@gmail.com>
2024-01-18 18:34:44 -0300

    feat: adding day 8

---
+++ src/main/java/dev/vinisantosn/week2/day8/Day8.java
@@ -0,0 +1,51 @@
+package dev.vinisantosn.week2.day8;
+
+import java.util.*;
+
+public class Day8 {
+    public static void main(String[] args) {
+        String digits = "23";
+        List<String> result = letterCombinations(digits);
+
+        System.out.println(result);
+    }
+
+    public static List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return result;
+        }
+
+        Map<Character, String> digitToLetters = new HashMap<>();
+        digitToLetters.put('2', "abc");
+        digitToLetters.put('3', "def");
+        digitToLetters.put('4', "ghi");
+        digitToLetters.put('5', "jkl");
+        digitToLetters.put('6', "mno");
+        digitToLetters.put('7', "pqrs");
+        digitToLetters.put('8', "tuv");
+        digitToLetters.put('9', "wxyz");
+
+        generateCombinations(digits, 0, new StringBuilder(), digitToLetters, result);
+
+        return result;
+    }
+
+    private static void generateCombinations(String digits, int index, StringBuilder current, Map<Character, String> digitToLetters, List<String> result) {
+        if (index == digits.length()) {
+            result.add(current.toString());
+            System.out.println(current.toString());
+            return;
+        }
+
+        char digit = digits.charAt(index);
+        String letters = digitToLetters.get(digit);
+
+        for (char letter : letters.toCharArray()) {
+            current.append(letter);
+            generateCombinations(digits, index + 1, current, digitToLetters, result);
+            current.deleteCharAt(current.length() - 1);
+        }
+    }
+
+}
